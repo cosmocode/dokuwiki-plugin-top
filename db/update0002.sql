ab49e75d8c69045f3b5ded9efca0b7f605797a7b
Michael Große <grosse@cosmocode.de>
2015-02-17 19:41:11 +0100

    Use long way to update db-table

--- db/update0002.sql
+++ db/update0002.sql
@@ -1,3 +1,23 @@
-ALTER TABLE toppages
-ADD lang TEXT
-ADD month TEXT
+CREATE TEMPORARY TABLE toptemp (
+    page,
+    month,
+    lang,
+    value,
+    PRIMARY KEY(page)
+);
+
+INSERT INTO toptemp (page, month, lang, value) SELECT page, '', '', value FROM toppages;
+
+DROP TABLE toppages;
+
+CREATE TABLE toppages (
+    page,
+    month,
+    lang,
+    value,
+    PRIMARY KEY(page)
+);
+
+INSERT INTO toppages (page, month, lang, value) SELECT page, month, lang, value FROM toptemp;
+
+DROP TABLE toptemp;
